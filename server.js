186c690248bbcef2d2b121cf6f17e259344827f3
matt arias <mattwork07@gmail.com>
2022-02-27 04:15:02 +0000

    added post /api/users

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+let mongoose;
+try {
+  mongoose = require('mongoose')
+}catch (e){
+  console.log(e)
+}
 require('dotenv').config()
 
 app.use(cors())
@@ -11,15 +17,66 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+mongoose.connect(process.env.MONGO_KEY, {useNewUrlParser: true, useUnifiedTopology: true});  
+
+//mongo schema and instatiation
+const UserSchema = new mongoose.Schema({
+  username: String,
+  description: [String],
+  duration: Number,//[Number]
+  date: String
+})
+
+let User = mongoose.model("user", UserSchema)
+
+let arrayOfClients = [{username: 'matt', duration: 28 , description:['Push-ups','Push-ups']},
+{username: 'camila', duration: 21 , description:['Squats','Push-ups']},
+{username: 'tadeo', duration: 29 , description:['Push-ups','Squats']},
+{username: 'maria', duration: 48 , description:['Push-ups','Squats']},
+{username: 'andres', duration: 38 , description:['Squats','Push-ups']}]
+
+// User.create(arrayOfClients,function(error,data){
+//     if (error) return console.log(error)
+// })
+
 app.post('/api/users/:username', (req, res) => {
+  let userToPost = req.body.username
+  let idToResponse;
+  let CreateOneUser = (done) => {
+    let newUser = new user({username: userToPost});
+    newUser.save(function(error, data){
+      if (error) return console.log(error)
+      done(null, data)
+    })
+  }
+  User.find({username:/matt/i}, '_id',(error, data) => {
+    if (error) return console.log(error)
+    console.log('entre en la busqueda')
+    idToResponse = data;
+  })
   res.json({
       username:  req.body.username,
-      _id: //id de la conexion con mongo
+      _id: idToResponse //id de la conexion con mongo
   })
+})
+
+app.post('/api/users', (req, res) => {
+  
+  User.find({}, '_id, username',(error, data) => {
+    if (error) return console.log(error)
+    console.log('entre en la busqueda2')
+    // idToResponse = data;
+    res.json({
+      username:  req.body.username,
+      _id: data //id de la conexion con mongo
+    })
+  })
+  
 })
 
 app.get('/api/users', (req,res) => {
   res.json({
+    hola:'hola'
     //mostrar todo los usuario registrados en un array
     //[{user,_id},{},{},{},]
   })
@@ -28,7 +85,7 @@ app.get('/api/users', (req,res) => {
 app.post('/api/users/:_id/exercises', (req, res) => {
   res.json({
       username:  req.body.username,
-      _id: //id de la conexion con mongo
+      _id: 'x'//id de la conexion con mongo
     // will be the user object with the exercise fields added.
   })
 })
